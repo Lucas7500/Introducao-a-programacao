8637d9b453ae6b33d69949dbfb84542c6e8c5095
Lucas <lucasiglesias2704@gmail.com>
2023-07-18 08:39:35 -0300

    bibliotecas proprias em c

---
+++ Minha-biblioteca/minha_bib.h
@@ -0,0 +1,21 @@
+#ifndef MINHA_BIB_H
+#define MINHA_BIB_H
+
+float maiorValor(float n1, float n2)
+{
+	if (n1 > n2)
+	{
+		return n1;
+	}
+	else
+	{
+		return n2;
+	}
+}
+
+float media(float n1, float n2)
+{
+	return (n1 + n2) / 2;
+}
+
+#endif
---
+++ Minha-biblioteca/teste-bib.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "E:\Programacao\Introducao-a-programacao\Minha-biblioteca\minha_bib.h"
+
+// Programa principal
+int main()
+{
+    printf("%f\n", media(2, 3));
+}
