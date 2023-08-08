790b37f4ba12ef29d17cce28a9c67a50127db7c7
Lucas <lucasiglesias2704@gmail.com>
2023-08-08 14:32:51 -0300

    Q6 DA ATIVIDADE AMEM

--- Atividades-Supervisionadas/Atividade-Avaliativa-Especial/AS56Q6.c
+++ Atividades-Supervisionadas/Atividade-Avaliativa-Especial/AS56Q6.c
@@ -7,33 +7,99 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct
 {
-    char nome[30];
+    char nome[100];
     float preco;
 } produtos;
 
 int main()
 {
+    char aux;
+    int i = 0, n = 1, k = 0;
+    char produto[50][100];
+    char precoTemp[10];
+    int len = 0;
+
     FILE *fr = fopen("produtos.txt", "r");
     if (fr == NULL)
     {
         printf("Erro ao abrir o arquivo");
         return 1;
     }
+    else
+    {
+        while (fscanf(fr, "%c", &aux) != EOF)
+        {
+            if (aux == '\n')
+            {
+                produto[i][k] = '\0';
+                i++;
+                k = 0;
+                continue;
+            }
+
+            produto[i][k] = aux;
+            k++;
+        }
+    }
+
+    len = i + 1;
+    k = 0;
+    i = 0;
 
     produtos *pdt;
-    pdt = (produtos *)malloc(sizeof(produtos));
+    pdt = (produtos *)malloc(n * sizeof(produtos));
     if (pdt == NULL)
     {
-        printf("Erro");
-        return 1;
+        printf("Nao ha memoria suficiente!");
+    }
+    else
+    {
+        for (int k = 0; k < len; k++)
+        {
+            for (int j = strlen(produto[k]); j >= 0; j--)
+            {
+                if (i >= n)
+                {
+                    pdt = (produtos *)realloc(pdt, (n + 1) * sizeof(produtos));
+                    n++;
+                }
+
+                if (produto[k][j] == ' ')
+                {
+                    for (int p = j + 1, idx = 0;; p++, idx++)
+                    {
+                        precoTemp[idx] = produto[k][p];
+
+                        if (produto[k][p] == '\0')
+                        {
+                            break;
+                        }
+                    }
+
+                    pdt[i].preco = atof(precoTemp);
+                    produto[k][j] = '\0';
+                    strcpy(pdt[i].nome, produto[k]);
+                    i++;
+                    break;
+                }
+            }
+        }
     }
 
-    while (1)
+    float total = 0;
+
+    for (int j = 0; j < len; j++)
     {
+        printf("%s %.2f\n", pdt[j].nome, pdt[j].preco);
+        total += pdt[j].preco;
     }
+
+    printf("TOTAL DA COMPRA: %.2f\n", total);
 
     return 0;
 }
